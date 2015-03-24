1e48ce047d4c4389fb047003e0aef730517f39ec
amin <m.amin.kashi@gmail.com>
2015-03-24 22:14:30 +0430

    singlePlayer & multiPlayer + battery Checking

--- nrf24l01/src/main.c
+++ nrf24l01/src/main.c
@@ -11,6 +11,8 @@
 #include "lcd.h"
 #include "transmitter.h"
 
+void packing_data(void);
+void stoping_robot(void);
 
 char Buf_Tx[2][Max_Robot][_Buffer_Size] ;
 char Buf_Rx_R[Max_Robot][_Buffer_Size];
@@ -29,6 +31,7 @@ int Robot_Select;
 int time;
 int timer=0;
 int data_flag = 0;
+bool battery_flag = false;
 
 uint16_t pck_timeout[2][Max_Robot];
 
@@ -47,22 +50,22 @@ int main (void)
 	NRF24L01_L_CE_LOW;       //disable transceiver modes
 	NRF24L01_R_CE_LOW;
 	/////////////////////////////////////////////////////////////spi setting
-																			
-	spi_xmega_set_baud_div(&NRF24L01_L_SPI,8000000UL,F_CPU);				
-	spi_enable_master_mode(&NRF24L01_L_SPI);								
-	spi_enable(&NRF24L01_L_SPI);											
-																			
-	spi_xmega_set_baud_div(&NRF24L01_R_SPI,8000000UL,F_CPU);				
-	spi_enable_master_mode(&NRF24L01_R_SPI);								
-	spi_enable(&NRF24L01_R_SPI);											
-																			
-	sei();																	
-																			
+	
+	spi_xmega_set_baud_div(&NRF24L01_L_SPI,8000000UL,F_CPU);
+	spi_enable_master_mode(&NRF24L01_L_SPI);
+	spi_enable(&NRF24L01_L_SPI);
+	
+	spi_xmega_set_baud_div(&NRF24L01_R_SPI,8000000UL,F_CPU);
+	spi_enable_master_mode(&NRF24L01_R_SPI);
+	spi_enable(&NRF24L01_R_SPI);
+	
+	sei();
+	
 	////////////////////////////////////////////////////////////////////////
 
 	_delay_us(10);
 	//power on reset delay needs 100ms
-	_delay_ms(100);															
+	_delay_ms(100);
 	NRF24L01_L_Clear_Interrupts();
 	NRF24L01_R_Clear_Interrupts();
 	NRF24L01_L_Flush_TX();
@@ -91,132 +94,121 @@ int main (void)
 	}
 	
 	while (1)
-	{		
+	{
+		
+		
 		
 	}
 }
 
 
 ISR(TCD0_OVF_vect)
-{   
+{
 	wdt_reset();
 	wireless_reset++;
 	time++;
-	if(time ==200)time=0;
+	if(time ==100)time=0;
+	
+// 		count = sprintf(str,"%d,%d,%d,%d\r",
+// 		((int)(Buf_Rx_R[Robot_Select][0]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][1]) & 0x0ff),
+// 		((int)(Buf_Rx_R[Robot_Select][2]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][3]) & 0x0ff),
+// 		((int)(Buf_Rx_R[Robot_Select][4]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][5]) & 0x0ff),
+// 		((int)(Buf_Rx_R[Robot_Select][6]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][7]) & 0x0ff));
+// 		for (uint8_t i=0;i<count;i++)
+// 		usart_putchar(&USARTE0,str[i]);
+	
+	if (time==1 && !(Menu_PORT.IN & Menu_Side_Select_PIN_bm) && battery_flag)
+	{
+		Robot_Select++;
+		if (Robot_Select < 3)
+		{
+			NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x01);
+		}
+		else
+		{
+			NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x00);
+		}
+			
+		if (Robot_Select > 2)
+		{
+			NRF24L01_R_WriteReg(W_REGISTER | EN_AA, 0x01);
+		}
+		else
+		{
+			NRF24L01_R_WriteReg(W_REGISTER | EN_AA, 0x00);
+		}
+
+		Buff_L[31] = Robot_Select;
+		Buff_R[31] = Robot_Select;
+
+
+		if (Robot_Select==6)
+		{
+			Robot_Select=0;
+			for(Robot_Select=0;Robot_Select<6;Robot_Select++)
+			{
+					int battery = ((int)(Buf_Rx_R[Robot_Select][14]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][15]) & 0x0ff);
+					count = sprintf(str,"Battery(%d):%d.%d  ",Robot_Select,battery/100,battery%100 );
+					
+					for (uint8_t i=0;i<count;i++)
+					usart_putchar(&USARTE0,str[i]);
+			}
+								count = sprintf(str,"\r");
+								for (uint8_t i=0;i<count;i++)
+								usart_putchar(&USARTE0,str[i]);
+								
+// 							 		NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x00);
+// 							 		NRF24L01_R_WriteReg(W_REGISTER | EN_AA, 0x00);
+// 							 		Buff_L[31] = '`';
+// 							 		Buff_R[31] = '`';
+			
+		}
+			
+		
+
+	}
 	
 	/////////////////////////////////packeting three robot data in one packet
-	Buff_L[0]='L';
-	for (int i = 1 ; i < 11 ; i++)
-	{
-		Buff_L[i] = Buf_Tx[R][0][i] ;
-	}
-
-	for (int i = 1 ; i < 11 ; i++)
-	{
-		Buff_L[i+10] = Buf_Tx[R][1][i] ;
-	}
-	
-	for (int i = 1 ; i < 11 ; i++)
-	{
-		Buff_L[i+20] = Buf_Tx[R][2][i] ;
-	}
-
-	Buff_R[0]='L';
-	for (int i = 1 ; i < 11 ; i++)
-	{
-		Buff_R[i] = Buf_Tx[L][3][i] ;
-	}
-
-	for (int i = 1 ; i < 11 ; i++)
-	{
-		Buff_R[i+10] = Buf_Tx[L][4][i] ;
-	}
-	
-	for (int i = 1 ; i < 11 ; i++)
-	{
-		Buff_R[i+20] = Buf_Tx[L][5][i] ;
-	}
-	//////////////////////////////////////////////////////////////////////////
+	packing_data();
 
 	//////////////////////////////////////////////////////////////////////////sending packet
-// 	if (wireless_reset==1)
-// 	{
-			NRF24L01_L_Write_TX_Buf(Buff_L, _Buffer_Size);
-			NRF24L01_L_RF_TX();
-			
-			NRF24L01_R_Write_TX_Buf(Buff_R, _Buffer_Size);
-			if (Buff_R[29]!=0)
-			{
-				LED_White_R_PORT.OUTSET = LED_White_R_PIN_bm;
-			}
+	// 	if (wireless_reset==1)
+	// 	{
+	NRF24L01_L_Write_TX_Buf(Buff_L, _Buffer_Size);
+	NRF24L01_L_RF_TX();
+	
+	NRF24L01_R_Write_TX_Buf(Buff_R, _Buffer_Size);
+	if (Buff_R[29]!=0)
+	{
+		LED_White_R_PORT.OUTSET = LED_White_R_PIN_bm;
+	}
 
-			NRF24L01_R_RF_TX();
-		wireless_reset = 0;
-/*	}*/
+	NRF24L01_R_RF_TX();
+	wireless_reset = 0;
+	/*	}*/
 	////////////////////////////////////////////////////////////////////////////////////////
 	
-	
+stoping_robot();
 
-	for (uint8_t i=0;i<Max_Robot;i++)
-	{
-		int side;
-		if(i<3)side = R;
-		if(i>2)side = L;
-		//if there is no order from pc for a robot
-		//an order of brake and then free wheel will
-		//be send to that robot
-		pck_timeout[side][i]++;
-		if (pck_timeout[side][i]>=300)
-		{
-			if(pck_timeout[side][i]<=600)
-			
-			{//brake order
-				Buf_Tx[side][i][0] = i;
-				Buf_Tx[side][i][1] = 0;
-				Buf_Tx[side][i][2] = 1;
-				Buf_Tx[side][i][3] = 0;
-				Buf_Tx[side][i][4] = 1;
-				Buf_Tx[side][i][5] = 0;
-				Buf_Tx[side][i][6] = 1;
-				Buf_Tx[side][i][7] = 0;
-				Buf_Tx[side][i][8] = 1;
-				Buf_Tx[side][i][9] = 0;
-				Buf_Tx[side][i][10] = 0;
-			}
-			else
-			{//freewheel order
-				Buf_Tx[side][i][0] = i;
-				Buf_Tx[side][i][1] = 1;
-				Buf_Tx[side][i][2] = 2;
-				Buf_Tx[side][i][3] = 3;
-				Buf_Tx[side][i][4] = 4;
-				Buf_Tx[side][i][5] = 0;
-				Buf_Tx[side][i][6] = time;
-				Buf_Tx[side][i][7] = 0;
-				Buf_Tx[side][i][8] = 0;
-				Buf_Tx[side][i][9] = 0;
-				Buf_Tx[side][i][10]= 0;
-			}
-		}
-	}
 	while(Menu_PORT.IN & Menu_Side_Switch_PIN_bm);
 	wdt_reset();
 }
 
-ISR(PRX_R)
-{		
+ISR(PRX_R)//ID:3,4,5
+{
 	wdt_reset();
 	uint8_t status_R = NRF24L01_R_ReadReg(STATUSe);
 	if((status_R & _RX_DR) == _RX_DR)
 	{
 		LED_White_R_PORT.OUTTGL = LED_White_R_PIN_bm;
-//		tmprid = ((status_R&0x0e)>>1);						
+		//		tmprid = ((status_R&0x0e)>>1);
 		//1) read payload through SPI,
 		NRF24L01_R_Read_RX_Buf(Buf_Rx_R[Robot_Select], _Buffer_Size);
 		//2) clear RX_DR IRQ,
 		status_R=NRF24L01_R_WriteReg(W_REGISTER | STATUSe, _RX_DR );
 		//3) read FIFO_STATUS to check if there are more payloads available in RX FIFO,
 		//4) if there are more data in RX FIFO, repeat from step 1).Buf_Tx[R]
+
 	}
 	
 	status_R = NRF24L01_R_WriteReg(W_REGISTER|STATUSe,_TX_DS|_MAX_RT);
@@ -231,14 +223,14 @@ ISR(PRX_R)
 	data_flag = 1;
 }
 
-ISR(PRX_L)
+ISR(PRX_L)//ID:0,1,2
 {
 	wdt_reset();
 	uint8_t status_L = NRF24L01_L_ReadReg(STATUSe);
 	if((status_L & _RX_DR) == _RX_DR)
 	{
 		LED_White_L_PORT.OUTTGL = LED_White_L_PIN_bm;
-//		tmprid = ((status_L&0x0e)>>1);						
+		//		tmprid = ((status_L&0x0e)>>1);
 		//1) read payload through SPI,
 		NRF24L01_L_Read_RX_Buf(Buf_Rx_R[Robot_Select], _Buffer_Size);
 		//2) clear RX_DR IRQ,
@@ -273,11 +265,207 @@ ISR(USART_R_DRE_vect) //Wireless_R_USART
 ISR(USART_L_RXC_vect)
 {
 
-GetNewData(USARTE0_DATA,L);
+	if (Menu_PORT.IN & Menu_Side_Select_PIN_bm)
+	{
+		GetNewData(USARTE0_DATA,L);
+	}
+	else
+	{
+		char data;
+		data=USARTE0_DATA;
+
+
+		switch (data)// used character : {w,s,123456(for robot id),!@#$(for motors id)}
+		{
+			
+			case 'w':
+			M3RPM+=10;
+			Buf_Tx[R][0][7] = (char)((M3RPM>>8) & 0x0ff) ;
+			Buf_Tx[R][0][8] = (char)(M3RPM & 0x0ff) ;
+			count = sprintf(str,"M3RPM: %d\r",M3RPM);
+			for (uint8_t i=0;i<count;i++)
+			usart_putchar(&USARTE0,str[i]);
+			break;
+			
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			Robot_Select = data - '0' ;
+			if (Robot_Select < 3)
+			{
+				NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x01);
+			}
+			else
+			{
+				NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x00);
+			}
+			
+			if (Robot_Select > 2)
+			{
+				NRF24L01_R_WriteReg(W_REGISTER | EN_AA, 0x01);
+			}
+			else
+			{
+				NRF24L01_R_WriteReg(W_REGISTER | EN_AA, 0x00);
+			}
+
+			Buff_L[31] = Robot_Select;
+			Buff_R[31] = Robot_Select;
+			break;
+			
+			case '`'://non of robots send ACK to wireless board
+			Robot_Select = 7 ;//- '0';
+			NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x00);
+			NRF24L01_R_WriteReg(W_REGISTER | EN_AA, 0x00);
+			Buff_L[31] = data;
+			Buff_R[31] = data;
+			break;
+			
+			case 'b':
+			case 'B':
+			battery_flag = true;
+			break;
+			
+			
+			
+		};
+		
+	}
 
 
 }
 ISR(USART_L_DRE_vect) //Wireless_R_USART
 {
 
+}
+
+void packing_data(void)
+{
+	if (Menu_PORT.IN & Menu_Side_Select_PIN_bm)
+	{
+		Buff_L[0]='L';
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_L[i] = Buf_Tx[R][0][i] ;
+		}
+
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_L[i+10] = Buf_Tx[R][1][i] ;
+		}
+		
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_L[i+20] = Buf_Tx[R][2][i] ;
+		}
+
+		Buff_R[0]='L';
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_R[i] = Buf_Tx[L][3][i] ;
+		}
+
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_R[i+10] = Buf_Tx[L][4][i] ;
+		}
+		
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_R[i+20] = Buf_Tx[L][5][i] ;
+		}
+		
+	}
+	else
+	{
+		Buff_L[0]='L';
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_L[i] = Buf_Tx[R][0][i] ;
+		}
+
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_L[i+10] = Buf_Tx[R][1][i] ;
+		}
+		
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_L[i+20] = Buf_Tx[R][2][i] ;
+		}
+
+		Buff_R[0]='L';
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_R[i] = Buf_Tx[R][3][i] ;
+		}
+
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_R[i+10] = Buf_Tx[R][4][i] ;
+		}
+		
+		for (int i = 1 ; i < 11 ; i++)
+		{
+			Buff_R[i+20] = Buf_Tx[R][5][i] ;
+		}
+	}
+	
+	
+}
+
+void stoping_robot(void)
+{
+	for (uint8_t i=0;i<Max_Robot;i++)
+	{
+		int side;
+		if (Menu_PORT.IN & Menu_Side_Select_PIN_bm)
+		{
+			if(i<3)side = R;
+			if(i>2)side = L;
+		}
+		else
+		{
+			side = R;
+		}
+
+		//if there is no order from pc for a robot
+		//an order of brake and then free wheel will
+		//be send to that robot
+		pck_timeout[side][i]++;
+		if (pck_timeout[side][i]>=300)
+		{
+			if(pck_timeout[side][i]<=600)
+			{//brake order
+				Buf_Tx[side][i][0] = i;
+				Buf_Tx[side][i][1] = 0;
+				Buf_Tx[side][i][2] = 1;
+				Buf_Tx[side][i][3] = 0;
+				Buf_Tx[side][i][4] = 1;
+				Buf_Tx[side][i][5] = 0;
+				Buf_Tx[side][i][6] = 1;
+				Buf_Tx[side][i][7] = 0;
+				Buf_Tx[side][i][8] = 1;
+				Buf_Tx[side][i][9] = 0;
+				Buf_Tx[side][i][10] = 0;
+			}
+			else
+			{//freewheel order
+				Buf_Tx[side][i][0] = i;
+				Buf_Tx[side][i][1] = 1;
+				Buf_Tx[side][i][2] = 2;
+				Buf_Tx[side][i][3] = 3;
+				Buf_Tx[side][i][4] = 4;
+				Buf_Tx[side][i][5] = 0;
+				Buf_Tx[side][i][6] = time;
+				Buf_Tx[side][i][7] = 0;
+				Buf_Tx[side][i][8] = 0;
+				Buf_Tx[side][i][9] = 0;
+				Buf_Tx[side][i][10]= 0;
+			}
+		}
+	}
 }
