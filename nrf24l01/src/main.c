7dab93675dabdbd3708261814c2edf4011cb9f41
amin <m.amin.kashi@gmail.com>
2015-03-24 11:34:46 +0430

    development

--- nrf24l01/src/main.c
+++ nrf24l01/src/main.c
@@ -12,7 +12,7 @@
 #include "transmitter.h"
 
 
-char Buf_Tx_R[Max_Robot][_Buffer_Size] ;
+char Buf_Tx[2][Max_Robot][_Buffer_Size] ;
 char Buf_Rx_R[Max_Robot][_Buffer_Size];
 char Buff_L[_Buffer_Size];
 char Buff_R[_Buffer_Size];
@@ -30,7 +30,7 @@ int time;
 int timer=0;
 int data_flag = 0;
 
-uint16_t pck_timeout[Max_Robot];
+uint16_t pck_timeout[2][Max_Robot];
 
 int main (void)
 {
@@ -87,25 +87,11 @@ int main (void)
 
 	for (uint8_t i=0;i<Max_Robot;i++)
 	{
-		Robot_D_tmp[i].RID=12;
+		Robot_D_tmp[2][i].RID=12;
 	}
 	
-	Buff_L[31]='N';
 	while (1)
-	{
-		if (data_flag ==1)
-		{
-// 			count = sprintf(str,"%d,%d,%d,%d,%d\r",
-// 			((int)(Buf_Rx_R[Robot_Select][0]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][1]) & 0x0ff),
-// 			((int)(Buf_Rx_R[Robot_Select][2]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][3]) & 0x0ff),
-// 			((int)(Buf_Rx_R[Robot_Select][4]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][5]) & 0x0ff),
-// 			((int)(Buf_Rx_R[Robot_Select][6]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][7]) & 0x0ff),
-// 			 time);
-// 			for (uint8_t i=0;i<count;i++)
-// 			usart_putchar(&USARTE0,str[i]);
-			//data_flag = 0 ;
-		}
-		
+	{		
 		
 	}
 }
@@ -122,33 +108,33 @@ ISR(TCD0_OVF_vect)
 	Buff_L[0]='L';
 	for (int i = 1 ; i < 11 ; i++)
 	{
-		Buff_L[i] = Buf_Tx_R[0][i] ;
+		Buff_L[i] = Buf_Tx[R][0][i] ;
 	}
 
 	for (int i = 1 ; i < 11 ; i++)
 	{
-		Buff_L[i+10] = Buf_Tx_R[1][i] ;
+		Buff_L[i+10] = Buf_Tx[R][1][i] ;
 	}
 	
 	for (int i = 1 ; i < 11 ; i++)
 	{
-		Buff_L[i+20] = Buf_Tx_R[2][i] ;
+		Buff_L[i+20] = Buf_Tx[R][2][i] ;
 	}
 
 	Buff_R[0]='L';
 	for (int i = 1 ; i < 11 ; i++)
 	{
-		Buff_R[i] = Buf_Tx_R[3][i] ;
+		Buff_R[i] = Buf_Tx[L][3][i] ;
 	}
 
 	for (int i = 1 ; i < 11 ; i++)
 	{
-		Buff_R[i+10] = Buf_Tx_R[4][i] ;
+		Buff_R[i+10] = Buf_Tx[L][4][i] ;
 	}
 	
 	for (int i = 1 ; i < 11 ; i++)
 	{
-		Buff_R[i+20] = Buf_Tx_R[5][i] ;
+		Buff_R[i+20] = Buf_Tx[L][5][i] ;
 	}
 	//////////////////////////////////////////////////////////////////////////
 
@@ -173,40 +159,43 @@ ISR(TCD0_OVF_vect)
 
 	for (uint8_t i=0;i<Max_Robot;i++)
 	{
+		int side;
+		if(i<3)side = R;
+		if(i>2)side = L;
 		//if there is no order from pc for a robot
 		//an order of brake and then free wheel will
 		//be send to that robot
-		pck_timeout[i]++;
-		if (pck_timeout[i]>=300)
+		pck_timeout[side][i]++;
+		if (pck_timeout[side][i]>=300)
 		{
-			if(pck_timeout[i]<=600)
+			if(pck_timeout[side][i]<=600)
 			
 			{//brake order
-				Buf_Tx_R[i][0] = i;
-				Buf_Tx_R[i][1] = 0;
-				Buf_Tx_R[i][2] = 1;
-				Buf_Tx_R[i][3] = 0;
-				Buf_Tx_R[i][4] = 1;
-				Buf_Tx_R[i][5] = 0;
-				Buf_Tx_R[i][6] = 1;
-				Buf_Tx_R[i][7] = 0;
-				Buf_Tx_R[i][8] = 1;
-				Buf_Tx_R[i][9] = 0;
-				Buf_Tx_R[i][10] = 0;
+				Buf_Tx[side][i][0] = i;
+				Buf_Tx[side][i][1] = 0;
+				Buf_Tx[side][i][2] = 1;
+				Buf_Tx[side][i][3] = 0;
+				Buf_Tx[side][i][4] = 1;
+				Buf_Tx[side][i][5] = 0;
+				Buf_Tx[side][i][6] = 1;
+				Buf_Tx[side][i][7] = 0;
+				Buf_Tx[side][i][8] = 1;
+				Buf_Tx[side][i][9] = 0;
+				Buf_Tx[side][i][10] = 0;
 			}
 			else
 			{//freewheel order
-				Buf_Tx_R[i][0] = i;
-				Buf_Tx_R[i][1] = 1;
-				Buf_Tx_R[i][2] = 2;
-				Buf_Tx_R[i][3] = 3;
-				Buf_Tx_R[i][4] = 4;
-				Buf_Tx_R[i][5] = 0;
-				Buf_Tx_R[i][6] = time;
-				Buf_Tx_R[i][7] = 0;
-				Buf_Tx_R[i][8] = 0;
-				Buf_Tx_R[i][9] = 0;
-				Buf_Tx_R[i][10]= 0;
+				Buf_Tx[side][i][0] = i;
+				Buf_Tx[side][i][1] = 1;
+				Buf_Tx[side][i][2] = 2;
+				Buf_Tx[side][i][3] = 3;
+				Buf_Tx[side][i][4] = 4;
+				Buf_Tx[side][i][5] = 0;
+				Buf_Tx[side][i][6] = time;
+				Buf_Tx[side][i][7] = 0;
+				Buf_Tx[side][i][8] = 0;
+				Buf_Tx[side][i][9] = 0;
+				Buf_Tx[side][i][10]= 0;
 			}
 		}
 	}
@@ -227,15 +216,7 @@ ISR(PRX_R)
 		//2) clear RX_DR IRQ,
 		status_R=NRF24L01_R_WriteReg(W_REGISTER | STATUSe, _RX_DR );
 		//3) read FIFO_STATUS to check if there are more payloads available in RX FIFO,
-		//4) if there are more data in RX FIFO, repeat from step 1).Buf_Tx_R
-						count = sprintf(str,"%d,%d,%d,%d,%d\r",
-						((int)(Buf_Rx_R[Robot_Select][0]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][1]) & 0x0ff),
-						((int)(Buf_Rx_R[Robot_Select][2]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][3]) & 0x0ff),
-						((int)(Buf_Rx_R[Robot_Select][4]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][5]) & 0x0ff),
-						((int)(Buf_Rx_R[Robot_Select][6]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][7]) & 0x0ff),
-						time);
-						for (uint8_t i=0;i<count;i++)
-						usart_putchar(&USARTE0,str[i]);
+		//4) if there are more data in RX FIFO, repeat from step 1).Buf_Tx[R]
 	}
 	
 	status_R = NRF24L01_R_WriteReg(W_REGISTER|STATUSe,_TX_DS|_MAX_RT);
@@ -264,14 +245,7 @@ ISR(PRX_L)
 		status_L=NRF24L01_L_WriteReg(W_REGISTER | STATUSe, _RX_DR );
 		//3) read FIFO_STATUS to check if there are more payloads available in RX FIFO,
 		//4) if there are more data in RX FIFO, repeat from step 1).
-						count = sprintf(str,"%d,%d,%d,%d,%d\r",
-						((int)(Buf_Rx_R[Robot_Select][0]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][1]) & 0x0ff),
-						((int)(Buf_Rx_R[Robot_Select][2]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][3]) & 0x0ff),
-						((int)(Buf_Rx_R[Robot_Select][4]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][5]) & 0x0ff),
-						((int)(Buf_Rx_R[Robot_Select][6]<<8) & 0xff00) | ((int)(Buf_Rx_R[Robot_Select][7]) & 0x0ff),
-						time);
-						for (uint8_t i=0;i<count;i++)
-						usart_putchar(&USARTE0,str[i]);
+
 	}
 	
 	status_L = NRF24L01_L_WriteReg(W_REGISTER|STATUSe,_TX_DS|_MAX_RT);
@@ -288,7 +262,7 @@ ISR(PRX_L)
 
 ISR(USART_R_RXC_vect)
 {
-	GetNewData(USARTC0_DATA);
+	GetNewData(USARTC0_DATA,R);
 }
 
 ISR(USART_R_DRE_vect) //Wireless_R_USART
@@ -298,80 +272,9 @@ ISR(USART_R_DRE_vect) //Wireless_R_USART
 
 ISR(USART_L_RXC_vect)
 {
-	char data;
-	data=USARTE0_DATA;
 
+GetNewData(USARTE0_DATA,L);
 
-	switch (data)// used character : {w,s,123456(for robot id),!@#$(for motors id)}
-	{
-		
-		case 'w':
-		M3RPM+=10;
-		Buf_Tx_R[0][7] = (char)((M3RPM>>8) & 0x0ff) ;
-		Buf_Tx_R[0][8] = (char)(M3RPM & 0x0ff) ;
-		count = sprintf(str,"M3RPM: %d\r",M3RPM);
-		for (uint8_t i=0;i<count;i++)
-		usart_putchar(&USARTE0,str[i]);
-		break;
-		
-		case '0':
-		case '1':
-		case '2':
-		case '3':
-		case '4':
-		case '5':
-		Robot_Select = data - '0' ;
-		if (Robot_Select < 3)
-		{
-			NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x01);
-		}
-		else
-		{
-			NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x00);
-		}
-		
-		if (Robot_Select > 2)
-		{
-			NRF24L01_R_WriteReg(W_REGISTER | EN_AA, 0x01);
-		}
-		else
-		{
-			NRF24L01_R_WriteReg(W_REGISTER | EN_AA, 0x00);
-		}
-
-		Buff_L[31] = Robot_Select;
-		Buff_R[31] = Robot_Select;
-		break;
-		
-		case '`'://non of robots send ACK to wireless board 
-		Robot_Select = 7 ;//- '0';
-		NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x00);
-		NRF24L01_R_WriteReg(W_REGISTER | EN_AA, 0x00);
-		Buff_L[31] = data;
-		Buff_R[31] = data;
-		break;
-		
-		case '!':
-//		Robot_Motor = 0;
-// 		NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x00);
-// 		Buff_L[31] = 0;
-		break;
-		
-		case '@':
-//		Robot_Motor = 1;
-// 		Buff_L[31] = 1;
-// 		NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x01);
-		break;
-		
-		case '#':
-//		Robot_Motor = 2;
-		break;
-		
-		case '$':
-//		Robot_Motor = 3;
-		break;
-		
-	};
 
 }
 ISR(USART_L_DRE_vect) //Wireless_R_USART
--- nrf24l01/src/transmitter.h
+++ nrf24l01/src/transmitter.h
@@ -21,11 +21,13 @@
 #define START_BYTE0 0xA5
 #define START_BYTE1 0x5A
 #define STOP_BYTE 0x80
-uint8_t PCK_Num=0;
+#define R 0
+#define L 1
+uint8_t PCK_Num[2]={0,0};
 uint8_t data_write_index=0,data_read_index=0,Packet_Next_Start=0;
 uint16_t PCK_Speed,PCK_Reset=0;
-extern char Buf_Tx_R[Max_Robot][_Buffer_Size];
-extern uint16_t pck_timeout[Max_Robot];
+extern char Buf_Tx[2][Max_Robot][_Buffer_Size];
+extern uint16_t pck_timeout[2][Max_Robot];
 struct PCK_Header
 {
 	uint8_t NOB;  //Counter for Packet Bytes
@@ -72,140 +74,143 @@ struct Robot_Send_Data
 	uint8_t EN4;
 };
 
-struct PCK_Header PCK_H;
-struct Robot_Data Robot_D[Max_Robot], Robot_D_tmp[Max_Robot];
+struct PCK_Header PCK_H[2];
+struct Robot_Data Robot_D[2][Max_Robot], Robot_D_tmp[2][Max_Robot];
 
 struct PCK_Send_Header PCK_S_H;
 struct Robot_Send_Data Robot_S_D, Robot_S_D_tmp, Robot_S_D_tmp2;
 uint8_t Robot_Send_PCK[11],Send_cnt=0;
 
-static void GetNewData(uint8_t data)
+static void GetNewData(uint8_t data,int side)
 {	
-	if (PCK_Num<Header_Lenght)
+	if (PCK_Num[side]<Header_Lenght)
 	{
-		switch(PCK_Num)
+		switch(PCK_Num[side])
 		{	
 		case 0:
 			if (data == START_BYTE0) 
 			{
-				PCK_Num++;// LED_White_R_PORT.OUTTGL = LED_White_R_PIN_bm;
+				PCK_Num[side]++;// LED_White_R_PORT.OUTTGL = LED_White_R_PIN_bm;
 			}
 			break;
 		case 1:
 			if (data == START_BYTE1) 
-				PCK_Num++;
+				PCK_Num[side]++;
 			else
 			{
-				PCK_Num = 0;
+				PCK_Num[side] = 0;
 			}			
 			break;
 		case 2:
-			PCK_H.SIB = data;
-			PCK_Num++;
+			PCK_H[side].SIB = data;
+			PCK_Num[side]++;
 			break;
 		case 3:
-			PCK_H.CHK = data;
-			PCK_Num++;
+			PCK_H[side].CHK = data;
+			PCK_Num[side]++;
 			break;
 		}
 	}	
 	else
 	{
-		if (PCK_Num < PCK_H.SIB-1)
+		if (PCK_Num[side] < PCK_H[side].SIB-1)
 		{
 			
-			switch((PCK_Num-Header_Lenght) % Data_Lenght) //0x07 is Size of Robot Data
+			switch((PCK_Num[side]-Header_Lenght) % Data_Lenght) //0x07 is Size of Robot Data
 			{
 			case 0:
-				Robot_D_tmp[(PCK_Num-Header_Lenght)/Data_Lenght].RID=data;
-				PCK_H.CHK -= data;
+				Robot_D_tmp[side][(PCK_Num[side]-Header_Lenght)/Data_Lenght].RID=data;
+				PCK_H[side].CHK -= data;
 				break;
 			case 1:
-				Robot_D_tmp[(PCK_Num-Header_Lenght)/Data_Lenght].M0.Bytes[1]=data;
-				PCK_H.CHK -= data;
+				Robot_D_tmp[side][(PCK_Num[side]-Header_Lenght)/Data_Lenght].M0.Bytes[1]=data;
+				PCK_H[side].CHK -= data;
 				break;
 			case 2:
-				Robot_D_tmp[(PCK_Num-Header_Lenght)/Data_Lenght].M0.Bytes[0]=data;
-				PCK_H.CHK -= data;
+				Robot_D_tmp[side][(PCK_Num[side]-Header_Lenght)/Data_Lenght].M0.Bytes[0]=data;
+				PCK_H[side].CHK -= data;
 				break;
 			case 3:
-				Robot_D_tmp[(PCK_Num-Header_Lenght)/Data_Lenght].M1.Bytes[1]=data;
-				PCK_H.CHK -= data;
+				Robot_D_tmp[side][(PCK_Num[side]-Header_Lenght)/Data_Lenght].M1.Bytes[1]=data;
+				PCK_H[side].CHK -= data;
 				break;
 			case 4:
-				Robot_D_tmp[(PCK_Num-Header_Lenght)/Data_Lenght].M1.Bytes[0]=data;
-				PCK_H.CHK -= data;
+				Robot_D_tmp[side][(PCK_Num[side]-Header_Lenght)/Data_Lenght].M1.Bytes[0]=data;
+				PCK_H[side].CHK -= data;
 				break;
 			case 5:
-				Robot_D_tmp[(PCK_Num-Header_Lenght)/Data_Lenght].M2.Bytes[1]=data;
-				PCK_H.CHK -= data;
+				Robot_D_tmp[side][(PCK_Num[side]-Header_Lenght)/Data_Lenght].M2.Bytes[1]=data;
+				PCK_H[side].CHK -= data;
 				break;
 			case 6:
-			Robot_D_tmp[(PCK_Num-Header_Lenght)/Data_Lenght].M2.Bytes[0]=data;
-			PCK_H.CHK -= data;
+			Robot_D_tmp[side][(PCK_Num[side]-Header_Lenght)/Data_Lenght].M2.Bytes[0]=data;
+			PCK_H[side].CHK -= data;
 			break;
 			case 7:
-			Robot_D_tmp[(PCK_Num-Header_Lenght)/Data_Lenght].M3.Bytes[1]=data;
-			PCK_H.CHK -= data;
+			Robot_D_tmp[side][(PCK_Num[side]-Header_Lenght)/Data_Lenght].M3.Bytes[1]=data;
+			PCK_H[side].CHK -= data;
 			break;
 			case 8:
-			Robot_D_tmp[(PCK_Num-Header_Lenght)/Data_Lenght].M3.Bytes[0]=data;
-			PCK_H.CHK -= data;
+			Robot_D_tmp[side][(PCK_Num[side]-Header_Lenght)/Data_Lenght].M3.Bytes[0]=data;
+			PCK_H[side].CHK -= data;
 			break;
 			case 9:
-			Robot_D_tmp[(PCK_Num-Header_Lenght)/Data_Lenght].KCK=data;
-			PCK_H.CHK -= data;
+			Robot_D_tmp[side][(PCK_Num[side]-Header_Lenght)/Data_Lenght].KCK=data;
+			PCK_H[side].CHK -= data;
 			break;
 			case 10:
-			Robot_D_tmp[(PCK_Num-Header_Lenght)/Data_Lenght].CHP=data;
-			PCK_H.CHK -= data;
+			Robot_D_tmp[side][(PCK_Num[side]-Header_Lenght)/Data_Lenght].CHP=data;
+			PCK_H[side].CHK -= data;
 			break;		
 			}
-			PCK_Num++;
+			PCK_Num[side]++;
 		}		
 		else
 		{ 
-			if (PCK_H.CHK == 0 && data == 0x80)
+			if (PCK_H[side].CHK == 0 && data == 0x80)
 			{	
 				PCK_Reset = 0;
 				PCK_Speed++;
 			
 				for (uint8_t i=0;i<Max_Robot;i++)
 				{
-					
-					if (Robot_D_tmp[i].RID!=12)
+					if (side == R && i<3 || side == L && i>2)
 					{
-						Robot_D[i] = Robot_D_tmp[i];
-						Robot_D_tmp[i].RID=12;
-						Robot_D_tmp[i].M0.Speed=0x7FFF;
-						Robot_D_tmp[i].M1.Speed=0x7FFF;
-						Robot_D_tmp[i].M2.Speed=0x7FFF;
-						Robot_D_tmp[i].M3.Speed=0x7FFF;
-						Robot_D_tmp[i].KCK=0;
-						Robot_D_tmp[i].CHP=0;
-
-						
-						if (Robot_D[i].RID<Max_Robot)
+					
+					
+						if (Robot_D_tmp[side][i].RID!=12)
 						{
-							Buf_Tx_R[Robot_D[i].RID][0] = Robot_D[i].RID;
-							Buf_Tx_R[Robot_D[i].RID][1] = Robot_D[i].M0.Bytes[1];
-							Buf_Tx_R[Robot_D[i].RID][2] = Robot_D[i].M0.Bytes[0];
-							Buf_Tx_R[Robot_D[i].RID][3] = Robot_D[i].M1.Bytes[1];
-							Buf_Tx_R[Robot_D[i].RID][4] = Robot_D[i].M1.Bytes[0];
-							Buf_Tx_R[Robot_D[i].RID][5] = Robot_D[i].M2.Bytes[1];
-							Buf_Tx_R[Robot_D[i].RID][6] = Robot_D[i].M2.Bytes[0];
-							Buf_Tx_R[Robot_D[i].RID][7] = Robot_D[i].M3.Bytes[1];
-							Buf_Tx_R[Robot_D[i].RID][8] = Robot_D[i].M3.Bytes[0];
-							Buf_Tx_R[Robot_D[i].RID][9] = Robot_D[i].KCK;
-							Buf_Tx_R[Robot_D[i].RID][10] = Robot_D[i].CHP;
+							Robot_D[side][i] = Robot_D_tmp[side][i];
+							Robot_D_tmp[side][i].RID=12;
+							Robot_D_tmp[side][i].M0.Speed=0x7FFF;
+							Robot_D_tmp[side][i].M1.Speed=0x7FFF;
+							Robot_D_tmp[side][i].M2.Speed=0x7FFF;
+							Robot_D_tmp[side][i].M3.Speed=0x7FFF;
+							Robot_D_tmp[side][i].KCK=0;
+							Robot_D_tmp[side][i].CHP=0;
 
-							pck_timeout[Robot_D[i].RID]=0;
-						}						
+							
+							if (Robot_D[side][i].RID<Max_Robot)
+							{
+								Buf_Tx[side][Robot_D[side][i].RID][0] = Robot_D[side][i].RID;
+								Buf_Tx[side][Robot_D[side][i].RID][1] = Robot_D[side][i].M0.Bytes[1];
+								Buf_Tx[side][Robot_D[side][i].RID][2] = Robot_D[side][i].M0.Bytes[0];
+								Buf_Tx[side][Robot_D[side][i].RID][3] = Robot_D[side][i].M1.Bytes[1];
+								Buf_Tx[side][Robot_D[side][i].RID][4] = Robot_D[side][i].M1.Bytes[0];
+								Buf_Tx[side][Robot_D[side][i].RID][5] = Robot_D[side][i].M2.Bytes[1];
+								Buf_Tx[side][Robot_D[side][i].RID][6] = Robot_D[side][i].M2.Bytes[0];
+								Buf_Tx[side][Robot_D[side][i].RID][7] = Robot_D[side][i].M3.Bytes[1];
+								Buf_Tx[side][Robot_D[side][i].RID][8] = Robot_D[side][i].M3.Bytes[0];
+								Buf_Tx[side][Robot_D[side][i].RID][9] = Robot_D[side][i].KCK;
+								Buf_Tx[side][Robot_D[side][i].RID][10] = Robot_D[side][i].CHP;
+
+								pck_timeout[side][Robot_D[side][i].RID]=0;
+							}
+						}
 					}
 				}
-				//LED_Green_R_PORT.OUTTGL = LED_Green_R_PIN_bm;
 			}
-			PCK_Num = 0;
+			PCK_Num[side] = 0;
 		}					
 	}
 }
